99065a72109cf1c9103894595f887db0de06b57b
Vinicius Civali <viniciuscivali@gmail.com>
2025-09-24 21:47:19 -0300

    simple_fast_api

---
+++ main.py
@@ -0,0 +1,48 @@
+from typing import List
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    text: str
+    is_done: bool = False
+
+
+items = []
+
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+
+@app.get("/items", response_model=List[Item])
+def return_items(limit: int = 10):
+    return items[0:limit]
+
+
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id: int) -> Item:
+    if item_id < len(items):
+        item = items[item_id]
+        return item
+    else:
+        raise HTTPException(status_code=404, detail="[404]: Item not found")
+
+
+@app.post("/items")
+def create_item(item: Item):
+    items.append(item)
+    return items
+
+
+@app.delete("/items")
+def delete_item(item: Item):
+    if item in items:
+        items.remove(item)
+        return items
+    else:
+        return f"{item} is not in items list"
